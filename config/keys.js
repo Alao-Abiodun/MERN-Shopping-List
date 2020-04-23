fb1f91b0af29c5cf2ecb433ffecf0e178af5383e
Alao-Abiodun <abiodunDev@gmail.com>
2020-04-23 17:11:16 +0100

    first-commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGO_URI:
+    "mongodb+srv://alao-abiodun:alao1996@cluster0-2bzt4.mongodb.net/test?retryWrites=true&w=majority",
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const items = require("./routes/api/items");
+
+const app = express();
+
+// Bodyparser middleware
+app.use(bodyParser.json());
+
+// DB config
+const db = require("./config/keys").MONGO_URI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+// Use Routes
+app.use("/api/items", items);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
